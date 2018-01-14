cad48c6edffee58cef9add3651f8cb641b3c2f54
Rahel <raheline@hotmail.com>
2018-01-14 11:34:59 +0200

    Some kind of space fix.

--- src/js/historyProvider.js
+++ src/js/historyProvider.js
@@ -1,34 +1,34 @@
 Appy.factory('historyProvider', function () {
-	
+
 	var scope = {};
-	
+
 	var history = [];
 	var historyStep = [];
-	
+
 	scope.startStep = function (cell1, cell2) {
 		historyStep = [makeHistoryObject(cell1), makeHistoryObject(cell2)];
 	};
-	
+
 	scope.setBlankVar = function (i, value) {
 		historyStep[i].isMadeBlank = value;
 	};
-	
+
 	scope.ready = function () {
 		history.push(historyStep);
-		
+
 		if (history.length > 10) {
 			history.shift();
 		}
 	};
-	
+
 	var makeHistoryObject = function (cell) {
 		return {cell: cell, val: cell.val, isMadeBlank: null};
 	};
-	
+
 	scope.undo = function () {
 		var step = history.pop();
 		if (step) {
-		
+
 			for (var i = 0; i < step.length; i++) {
 				var cell = step[i].cell;
 				cell.val = step[i].val;
@@ -39,7 +39,7 @@ Appy.factory('historyProvider', function () {
 			}
 		}
 	};
-	
-	
+
+
 	return scope;
 });
