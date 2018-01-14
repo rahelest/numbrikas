3477ea28ea3b2d24f4bf11de5ec8eaaf1da98d0b
Rahel <raheline@hotmail.com>
2018-01-14 17:09:27 +0200

    Add eslintrc and fix bunch of stuff.

--- src/js/autoSaveProvider.js
+++ src/js/autoSaveProvider.js
@@ -1,29 +1,30 @@
-Appy.factory('autoSaveProvider', ["loadSaveProvider", "localStorageService", function (loadSaveProvider, localStorageService) {
+Appy.factory("autoSaveProvider", ["loadSaveProvider", "localStorageService", function(loadSaveProvider, localStorageService) {
+    var scope = {};
 
-	var scope = {};
+    scope.loadSuccess = function() {
+        if (localStorageService.isSupported) {
+            try {
+                var data = localStorageService.get("save");
+                loadSaveProvider.importTable(data);
+                return true;
+            }
+            catch (e) {
+                // Porn mode, I guess.
+            }
+        }
+    };
 
-	scope.loadSuccess = function () {
-		if (localStorageService.isSupported) {
-			try {
-				var data = localStorageService.get("save");
-				loadSaveProvider.importTable(data);
-				return true;
-			} catch (e) {
-				console.log(e);
-			}
-		}
-	}
+    scope.save = function() {
+        if (localStorageService.isSupported) {
+            try {
+                var stringData = loadSaveProvider.exportTable();
+                localStorageService.set("save", stringData);
+            }
+            catch (e) {
+                // Porn mode, I guess.
+            }
+        }
+    };
 
-	scope.save = function (data) {
-		if (localStorageService.isSupported) {
-			try {
-				var stringData = loadSaveProvider.exportTable();
-				localStorageService.set("save", stringData);
-			} catch (e) {
-				console.log(e);
-			}
-		}
-	}
-
-	return scope;
+    return scope;
 }]);
--- src/js/main.js
+++ src/js/main.js
@@ -1,75 +1,74 @@
-window.Appy = angular.module('numberApp', ["LocalStorageModule"]);
+window.Appy = angular.module("numberApp", ["LocalStorageModule"]);
 
 Appy.config(function(localStorageServiceProvider) {
     localStorageServiceProvider.setPrefix("numbers19");
 });
 
-Appy.controller("numberController", ['$scope', '$rootScope', 'pairChecker', 'historyProvider', 'helperService', 'loadSaveProvider', 'expandService', "autoSaveProvider",
-                                     function ($scope, $rootScope, pairChecker, history, helper, loadSave, expand, autoSaveProvider) {
-
-	/*
+Appy.controller("numberController", ["$scope", "$rootScope", "pairChecker", "historyProvider", "helperService",
+    "loadSaveProvider", "expandService", "autoSaveProvider",
+    function($scope, $rootScope, pairChecker, history, helper, loadSave, expand, autoSaveProvider) {
+        /*
 	 * Algus: 22:30
 	 * Valmis 1:30
 	 */
-     var init = [ {row: 0, data: [1, 2, 3, 4, 5, 6, 7, 8, 9]},
-                  {row: 1, data: [1, 1, 1, 2, 1, 3, 1, 4, 1]},
-                  {row: 2, data: [5, 1, 6, 1, 7, 1, 8, 1, 9]}];
+        var init = [ {row: 0, data: [1, 2, 3, 4, 5, 6, 7, 8, 9]},
+            {row: 1, data: [1, 1, 1, 2, 1, 3, 1, 4, 1]},
+            {row: 2, data: [5, 1, 6, 1, 7, 1, 8, 1, 9]}];
 
-    initialize();
+        initialize();
 
-    if (!autoSaveProvider.loadSuccess()) {
-        loadSave.parseArray(init);
-    }
+        if (!autoSaveProvider.loadSuccess()) {
+            loadSave.parseArray(init);
+        }
 
-  connectHandlers();
+        connectHandlers();
 
-	var updateHelper = function () {
-		helper.update();
-	};
+        var updateHelper = function() {
+            helper.update();
+        };
 
-    function initialize() {
-        $rootScope.table = [];
-        $rootScope.rowBlank = [];
+        function initialize() {
+            $rootScope.table = [];
+            $rootScope.rowBlank = [];
 
-        $rootScope.helperNums = {};
-        $rootScope.firstHiddenRow = 0;
-    }
+            $rootScope.helperNums = {};
+            $rootScope.firstHiddenRow = 0;
+        }
 
-  function connectHandlers() {
-    $scope.select = pairChecker.select;
+        function connectHandlers() {
+            $scope.select = pairChecker.select;
 
-    $scope.restart =  function () {
-        loadSave.parseArray(init);
-        autoSaveProvider.save();
-    };
+            $scope.restart = function() {
+                loadSave.parseArray(init);
+                autoSaveProvider.save();
+            };
 
-    $scope.expand = function () {
-      expand.expand();
+            $scope.expand = function() {
+                expand.expand();
 
-      $scope.helperNeeded = true;
-      updateHelper();
+                $scope.helperNeeded = true;
+                updateHelper();
 
-      autoSaveProvider.save();
-    };
+                autoSaveProvider.save();
+            };
 
-    $scope.parse = function () {
-      var json = $scope.json;
-      loadSave.importTable(json);
+            $scope.parse = function() {
+                var json = $scope.json;
+                loadSave.importTable(json);
 
-      $scope.helperNeeded = true;
-      updateHelper();
-    };
+                $scope.helperNeeded = true;
+                updateHelper();
+            };
 
-    $scope.save = function () {
-      $scope.json = loadSave.exportTable();
-    };
+            $scope.save = function() {
+                $scope.json = loadSave.exportTable();
+            };
 
-    $scope.undo = history.undo;
+            $scope.undo = history.undo;
 
-    $(window).scroll(function () {
-      updateHelper();
-      $rootScope.$apply();
-    });
-  }
-
-}]);
+            $(window).scroll(function() {
+                updateHelper();
+                $rootScope.$apply();
+            });
+        }
+    }]);
